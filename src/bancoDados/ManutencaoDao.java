9d44d3a56a51ff23ebdfbde0b558764a5b684628
FilhoDeEd <edson.er.rodrigues@gmail.com>
2023-06-21 19:18:00 -0300

    Update ManutencaoDao.java

--- src/bancoDados/ManutencaoDao.java
+++ src/bancoDados/ManutencaoDao.java
@@ -1,21 +1,55 @@
 package bancoDados;
+
 import java.sql.SQLException;
 import manutencaoAerea.Manutencao;
+import java.util.List;
 
-public class ManutencaoDao extends GenericDao {
+
+public class ManutencaoDao extends GenericDao
+{
 	private Manutencao m;
-	public ManutencaoDao(Manutencao m) {
+	
+	public ManutencaoDao(Manutencao m)
+	{
 		this.m = m;
 	}
 	
-	public boolean inserir() {
-		try {
-			String comando = "insert into cliente values (?,?)";
-			inserir(comando, m.getCodigo(), m.getDataRealizacao());
+	public boolean inserir()
+	{
+		try
+		{
+			String comando = "INSERT INTO manutencao VALUES (?,?)";
+			super.inserir(comando, m.getCodigo(), m.getDataRealizacao());
 			return true;
-		}catch(SQLException e) {
+		}
+		catch(SQLException e)
+		{
 			e.printStackTrace();
 			return false;
 		}
 	}
+	
+	public List<Object[]> selecionar(String... camposSelect)
+	{
+        try
+        {
+        	String comando = "SELECT ";
+        	
+        	for(String campo : camposSelect)
+        	{
+        		comando = comando.concat(campo);
+        		comando = comando.concat(", ");
+        	}
+        	
+        	comando = comando.substring(0, comando.length() - 2);
+        	comando = comando.concat(" FROM manutencao");
+
+            return super.selecionar(comando);
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
