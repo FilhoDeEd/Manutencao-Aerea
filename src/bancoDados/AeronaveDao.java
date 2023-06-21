33e2c5c7d376102a8b07ac53fdecfe5b13e6cf03
FilhoDeEd <edson.er.rodrigues@gmail.com>
2023-06-21 19:18:02 -0300

    Create AeronaveDao.java

---
+++ src/bancoDados/AeronaveDao.java
@@ -0,0 +1,55 @@
+package bancoDados;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import manutencaoAerea.Aeronave;
+
+public class AeronaveDao extends GenericDao
+{
+	private Aeronave m;
+	
+	public AeronaveDao(Aeronave m)
+	{
+		this.m = m;
+	}
+	
+	public boolean inserir()
+	{
+		try
+		{
+			String comando = "INSERT INTO aeronave VALUES (?,?,?,?,?)";
+			super.inserir(comando, m.getPrefixo(), m.getModelo(), m.getFabricante(), m.getNumeroSerie(), m.getDataFabricacao());
+			return true;
+		}
+		catch(SQLException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public List<Object[]> selecionar(String... camposSelect)
+	{
+        try
+        {
+        	String comando = "SELECT ";
+        	
+        	for(String campo : camposSelect)
+        	{
+        		comando = comando.concat(campo);
+        		comando = comando.concat(", ");
+        	}
+        	
+        	comando = comando.substring(0, comando.length() - 2);
+        	comando = comando.concat(" FROM aeronave");
+
+            return super.selecionar(comando);
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
